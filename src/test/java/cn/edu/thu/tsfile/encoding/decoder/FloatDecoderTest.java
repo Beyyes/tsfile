e79cbea0a5bc783460753bba4c6801990e04ed12
CGF <cgf1993@foxmail.com>
2017-06-09 17:51:11 +0800

    modify FloatDecoderTest.java

--- src/test/java/cn/edu/thu/tsfile/encoding/decoder/FloatDecoderTest.java
+++ src/test/java/cn/edu/thu/tsfile/encoding/decoder/FloatDecoderTest.java
@@ -37,7 +37,7 @@ public class FloatDecoderTest {
   public void setUp() throws Exception {
     floatList = new ArrayList<Float>();
     int hybridCount = 11;
-    int hybridNum = 50;
+    int hybridNum = 30;
     int hybridStart = 20;
     for (int i = 0; i < hybridNum; i++) {
       for (int j = 0; j < hybridCount; j++) {
@@ -52,7 +52,7 @@ public class FloatDecoderTest {
 
     doubleList = new ArrayList<Double>();
     int hybridCountDouble = 11;
-    int hybridNumDouble = 50;
+    int hybridNumDouble = 30;
     long hybridStartDouble = 20;
 
     for (int i = 0; i < hybridNumDouble; i++) {
